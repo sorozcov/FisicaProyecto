ea08eab9b3318bf588f1b8f6c497263868110111
Maria Vasquez <maria.ines.vf@gmail.com>
2019-05-15 13:12:28 -0600

    bolas elasticas

---
+++ ProyectoFisica/Assets/BolasElasticas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolasElasticas : MonoBehaviour
+   
+{
+    private Rigidbody rb;
+    public float force = 20;
+    int dir=0;
+
+    public int direc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb)
+        {
+            if (dir==1) {
+                force = force * -1;
+            }
+            rb.AddForce(force, 0, 0, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
