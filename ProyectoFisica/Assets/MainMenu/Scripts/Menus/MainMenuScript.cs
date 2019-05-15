70c8de36da4f41a9cd1951bba43f0776d5427bdd
sorozcov <42949349+sorozcov@users.noreply.github.com>
2019-05-15 12:27:05 -0600

    MainMenu

---
+++ ProyectoFisica/Assets/MainMenu/Scripts/Menus/MainMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarEscena(string newscene) {
+        //Cambia de escena hacia donde queremos salir.
+        
+        SceneManager.LoadScene(newscene);
+    }
+    public void Quit()
+    {
+        //Si esta en editor apaga el editor, si esta en ejecutable cierra la aplicacion.
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+        else {
+            Application.Quit();
+        }
+    }
+}
